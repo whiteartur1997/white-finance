1bb4502aa01b56a602215dcc9e109a4af8057542
arturwhite1997 <whiteartur1997@gmail.com>
2023-05-04 23:32:53 +0300

    add route guards for /auth and /home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AuthComponent} from "./auth/auth.component";
 import {HomeComponent} from "./home/home.component";
+import {AuthGuardService} from "./auth/auth-guard.service";
+import {NoAuthGuardService} from "./auth/no-auth-guard.service";
 
 const routes: Routes = [
-  { path: 'auth', component: AuthComponent },
-  { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: 'home', pathMatch: "full" },
+  { path: 'auth', component: AuthComponent, canActivate: [NoAuthGuardService]  },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuardService] },
+  { path: '', redirectTo: '/home', pathMatch: "full" },
 ];
 
 @NgModule({
